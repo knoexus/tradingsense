df6aad6c7c1afabd417478acd3076678bffe6da4
knoexus <knoexus@yahoo.com>
2020-08-02 13:03:24 +0300

    changed number of retrievable proxies

--- upd_db/proxy.py
+++ upd_db/proxy.py
@@ -6,11 +6,12 @@ def get_proxies(delay=0):
     if delay > 0:
         time.sleep(delay)
     start_delay = 30
+    number_of_proxies = 20 # max 20 per page
     url = 'https://free-proxy-list.net/'
     response = requests.get(url)
     parser = fromstring(response.text)
     proxies = set()
-    for i in parser.xpath('//tbody/tr')[:10]:
+    for i in parser.xpath('//tbody/tr')[:number_of_proxies]:
         if i.xpath('.//td[7][contains(text(),"yes")]'):
             proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
             proxies.add(proxy)
