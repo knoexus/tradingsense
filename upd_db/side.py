381f25c65ad6bb3a031021739c87e73eb94aa5f9
knoexus <knoexus@yahoo.com>
2020-08-03 19:45:27 +0300

    added technicals

--- upd_db/side.py
+++ upd_db/side.py
@@ -4,11 +4,13 @@ import time
 import requests
 import os
 import proxy
+import json
+import inspect
 from requests.exceptions import ProxyError, SSLError
 
 def get_symbol_list():
     raw_symbols = requests.get(f"https://finnhub.io/api/v1/stock/symbol?exchange=US&token={os.environ['FINNHUB_API_KEY']}")
-    return list(filter(filter_symbol, raw_symbols.json()))
+    return tuple(filter(filter_symbol, raw_symbols.json()))
 
 def filter_symbol(x):
     if x["type"] == "EQS":
@@ -29,8 +31,26 @@ class DataOps:
     STATUS_LIMIT_EXCEEDED = 429
     STATUS_SUCCESS = 200
 
+    INIT_TIMESTAMP = 1260576000
+    CURRENT_TIMESTAMP = int(time.time())
+
+    # helper
+    def set_timeout_based_on_current_index(self, idx, max_requests=60, timeout_second=61):
+        if idx != 0 and idx % max_requests == 0:
+            print(f"{inspect.currentframe().f_code.co_name} timeout started")
+            time.sleep(timeout_second)
+    
+    def iterate_proxies(self, val, delay):
+        if val == len(self.proxies):
+            val == 0 
+            self.proxies = proxy.get_proxies(delay=delay)    
+        else:
+            val += 1
+
+    # data
     def call_all_authed(self):
-        methods = [self.get_recommendation_trends, self.get_financials_reported, self.get_earnings_calendar, self.get_candles, self.get_company_profile]
+        methods = [self.get_recommendation_trends, self.get_financials_reported, self.get_earnings_calendar, 
+                    self.get_candles, self.get_company_profile, self.get_technicals()]
         for method in methods:
             method()
             time.sleep(60)
@@ -42,8 +62,7 @@ class DataOps:
     def get_recommendation_trends(self):
         self.db.recommendation_trends.create_index([("period", 1), ("symbol", 1)], unique=True)
         for idx, item in enumerate(self.symbols):
-            if idx % 60 == 0:
-                time.sleep(61)
+            self.set_timeout_based_on_current_index(idx)
             symbol = item["symbol"]
             print(f"Downloading {symbol}")
             result = self.finnhub_client.recommendation_trends(symbol)
@@ -59,8 +78,7 @@ class DataOps:
     def get_financials_reported(self):
         self.db.financials_reported.create_index([("accessNumber", 1), ("symbol", 1)], unique=True)
         for idx, item in enumerate(self.symbols):
-            if idx % 60 == 0:
-                time.sleep(61)
+            self.set_timeout_based_on_current_index(idx)
             symbol = item["symbol"]
             print(f"Downloading {symbol}")
             result = self.finnhub_client.financials_reported(symbol=symbol, freq='quarterly')
@@ -76,8 +94,7 @@ class DataOps:
     def get_earnings_calendar(self):
         self.db.earnings_calendar.create_index([("date", 1), ("symbol", 1)], unique=True)
         for idx, item in enumerate(self.symbols):
-            if idx % 60 == 0:
-                time.sleep(61)
+            self.set_timeout_based_on_current_index(idx)
             symbol = item["symbol"]
             print(f"Downloading {symbol}")
             result = self.finnhub_client.earnings_calendar(symbol=symbol)
@@ -93,12 +110,10 @@ class DataOps:
     def get_candles(self):
         self.db.candles.create_index([("timestamp", 1), ("symbol", 1)], unique=True)
         for idx, item in enumerate(self.symbols):
-            if idx % 60 == 0:
-                time.sleep(61)
+            self.set_timeout_based_on_current_index(idx)
             symbol = item["symbol"]
             print(f"Downloading {symbol}")
-            current_timestamp = int(time.time())
-            result = self.finnhub_client.stock_candles(symbol=symbol, resolution="D", _from=1260576000, to=current_timestamp)
+            result = self.finnhub_client.stock_candles(symbol=symbol, resolution="D", _from=self.INIT_TIMESTAMP, to=self.CURRENT_TIMESTAMP)
             if result != {} and result["c"] != [] and result["t"] != []:
                 try:
                     transformed = []
@@ -124,8 +139,7 @@ class DataOps:
         self.db.company_profile.create_index([("symbol", 1), ("shareOutstanding", 1)], unique=True)
         data_list = []
         for idx, item in enumerate(self.symbols):
-            if idx % 60 == 0:
-                time.sleep(61)
+            self.set_timeout_based_on_current_index(idx)
             symbol = item["symbol"]
             print(f"Downloading {symbol}")
             result = self.finnhub_client.company_profile2(symbol=symbol)
@@ -138,21 +152,37 @@ class DataOps:
         except BulkWriteError as bwe:
             print(f"{self.get_company_profile.__name__}: BulkWrite - {symbol} - {bwe.details['writeErrors'][0]['errmsg']}")
 
-    def get_technicals(self):
-        pass
-
-    def get_pattern_recognition(self):
-        pass
-
+    def get_technicals(self, cut=30):
+        with open('technicals.json') as json_technicals:
+            technicals_dict = json.load(json_technicals)
+            count_for_timeout = 0
+            for item in self.symbols:
+                symbol = item["symbol"]
+                print(f"Downloading {symbol}")
+                data_obj = {
+                    "symbol": symbol,
+                    "data": {}
+                }
+                for idx, indicator in enumerate(technicals_dict):
+                    count_for_timeout += 1
+                    self.set_timeout_based_on_current_index(count_for_timeout) 
+                    indicator_alias = technicals_dict[indicator]
+                    result = self.finnhub_client.technical_indicator(symbol, "D", self.INIT_TIMESTAMP, self.CURRENT_TIMESTAMP, 
+                        indicator, indicator_alias["params"])
+                    if idx == 0:
+                        data_obj["data"]["t"] = result["t"][:-cut]
+                    else:
+                        print(f'Timestamp lists equality: {data_obj["data"]["t"] == result["t"][:-cut]}, lengths: {len(data_obj["data"]["t"])+cut} - {len(result["t"])}')
+                    for s in indicator_alias["retrievable_symbols"]:
+                        data_obj["data"][str.upper(s)] = result[s][:-cut]
+                data_obj["data"]["t"] = list(map(lambda x: datetime.datetime.fromtimestamp(x), data_obj["data"]["t"]))
+                try:
+                    self.db.technicals.replace_one({"symbol": symbol}, data_obj, upsert=True)
+                except BulkWriteError as bwe:
+                    print(f"{self.get_technicals.__name__}: BulkWrite - {symbol} - {bwe.details['writeErrors'][0]['errmsg']}")
+                
     # unauthed
     def get_financials(self):
-        def iterate_proxies(val, delay):
-            if val == len(self.proxies):
-                val == 0 
-                self.proxies = proxy.get_proxies(delay=delay)    
-            else:
-                val += 1
-
         self.db.financials.create_index([("period", 1), ("symbol", 1), ('accumulatedDepreciationTotal', 1)], unique=True)
         i = 0
         delay = 15
@@ -165,18 +195,18 @@ class DataOps:
             while status == self.STATUS_LIMIT_EXCEEDED:
                 print(self.proxies)
                 if self.proxies[i]["https"] in local_used_proxies:
-                    iterate_proxies(i, delay)
+                    self.iterate_proxies(i, delay)
                     continue
                 try: 
                     response = requests.get(f"https://finnhub.io/api/v1/stock/financials?symbol={symbol}&statement=bs&freq=quarterly", proxies=self.proxies[i])
                 except (ProxyError, SSLError):
                     print("Proxy error occured")
-                    iterate_proxies(i, delay)
+                    self.iterate_proxies(i, delay)
                     continue
                 status = response.status_code 
                 if status == self.STATUS_LIMIT_EXCEEDED:
                     local_used_proxies[self.proxies[i]["https"]] = self.proxies[i]
-                    iterate_proxies(i, delay)
+                    self.iterate_proxies(i, delay)
                 elif status != self.STATUS_SUCCESS:
                     print(f"{self.get_financials.__name__}: Error {status} - {symbol} occured")
                 else:
