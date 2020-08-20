07aec18e80c2790eebb8cb671c35c07c3eb82cde
knoexus <knoexus@yahoo.com>
2020-08-20 16:56:00 +0300

    added covering containers on locked items & some logic behind for CompanyProfile

--- web/client/src/components/CompanyProfile.js
+++ web/client/src/components/CompanyProfile.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import Lock from './util/Lock'
 import '../styles/main.css'
 
 export default function CompanyProfile( { data: { ticker, name, logo, exchange, finnhubIndustry } }) {
+    const lock = true
+    const renderItemBasedOnLockState = (state, element, divExtraClasses, lockSize) => 
+        !state ?
+        (
+            <div className="companyProfile-content-item">
+                { element }
+            </div>
+        ) : 
+        (
+            <div className={`companyProfile-content-item-locked ${divExtraClasses ? divExtraClasses.join(' ') : ''}`}>
+                <Lock size={lockSize}/>
+            </div>
+        )
     return (
         <div className="companyProfile">
             <div className="companyProfile-content">
                 <div className="companyProfile-logo">
-                    <img alt={"Logo"} src={logo}></img>
+                    { renderItemBasedOnLockState(lock, <img alt={"Logo"} src={logo}></img>) }
                 </div>
                 <div className="companyProfile-info">
-                    <h3>{name}</h3>
-                    <span>{exchange} / {ticker}</span>
-                    <span>{finnhubIndustry}</span>
+                    { renderItemBasedOnLockState(lock, 
+                    (
+                        <Fragment>
+                            <h3>{name}</h3> 
+                            <span>{exchange} / {ticker}</span>                              
+                        </Fragment>
+                    ), ['companyProfile-content-item-locked-name']) }
+                    { renderItemBasedOnLockState(lock, <span>{finnhubIndustry}</span>, ['companyProfile-content-item-locked-sector'], "xl") }
                 </div>
             </div>
         </div>
---
+++ web/client/src/components/util/Lock.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../styles/main.css'
+
+export default function Lock({size}) {
+    return (
+        <div className={`lock-icon lock-icon-${size ?? 'md'}`}>
+            <svg version="1.1" id="Layer_1" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64">
+                <g>
+                    <rect x="8" y="33" fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" width="48" height="30"/>
+                    <path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M16,33V17c0-8.837,7.163-16,16-16s16,7.163,16,16
+                        v16"/>
+                    <circle fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" cx="32" cy="47" r="4"/>
+                    <line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="32" y1="51" x2="32" y2="55"/>
+                </g>
+            </svg>
+        </div>
+    )
+}
