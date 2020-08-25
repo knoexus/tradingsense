20d4683af81f68c3e5ebb0d6fc53b05eab7c353e
knoexus <knoexus@yahoo.com>
2020-08-25 14:11:15 +0300

    added arrow tooltip

--- web/client/src/components/util/Arrow.js
+++ web/client/src/components/util/Arrow.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import Tooltip from '@material-ui/core/Tooltip'
+import Zoom from '@material-ui/core/Zoom'
 import '../../styles/arrowButton.scss'
 
 export default function Arrow({side, clicked}) {
     return (
-        <div onClick={clicked} className={`button-FastForward-${side}`}>
-            <div className="arrow">
-                <div className={`arrow-top arrow-top-${side}`}></div>
-                <div className={`arrow-bottom arrow-bottom-${side}`}></div>
+        <Tooltip 
+            TransitionComponent={Zoom} 
+            title={side == "left" ? "Sell" : "Buy"} 
+            placement={side == "left" ? "top-end" : "top-start"}>
+            <div onClick={clicked} className={`button-FastForward-${side}`}>
+                <div className="arrow">
+                    <div className={`arrow-top arrow-top-${side}`}></div>
+                    <div className={`arrow-bottom arrow-bottom-${side}`}></div>
+                </div>
             </div>
-        </div>
+        </Tooltip>
     )
 }
