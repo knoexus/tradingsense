6a1e330619e28884fe12568248c73b495396646c
knoexus <knoexus@yahoo.com>
2020-08-26 02:47:37 +0300

    header restyling w/ gradient + major chart changes (gap, endLine, markers, etc.)

--- web/client/src/components/util/LineChart.js
+++ web/client/src/components/util/LineChart.js
@@ -3,30 +3,54 @@ import moment from 'moment'
 import Chart from 'chart.js'
 
 export default function LineChart({data}) {
+    const predictionGap = 30
+
     const ctx = useRef()
     let dates = [], quotes = []
     data.forEach(e => {
         dates.push(moment(e.timestamp).format('DD/MM/YYYY').toString())
         quotes.push(e.close)
     })
+    const mockArray = Array.from(Array(predictionGap).map(e => null))
+    const quotesWithSpace = quotes.concat(mockArray)
+    const datesWithSpace = dates.concat(mockArray)
     const [myChart, setChart] = useState()
+
     const chart = () => {
         if (myChart) myChart.destroy()
         setChart(
             new Chart(ctx.current, {
                 type: 'line',
                 data: {
-                    labels: dates,
+                    labels: datesWithSpace,
                     datasets: [{
                         label: `${data.length}-day interval Daily Quotes`,
-                        data: quotes,
+                        data: quotesWithSpace,
                         fill: false,
                         borderColor: [
                             'rgba(244,0,0,0.8)'
                         ]
                     }]
                 },
+                plugins: [{
+                    afterDraw: function(chart) {
+                        const ctx = chart.ctx
+                        const x = 520-3
+                        const topY = chart.scales['y-axis-0'].top
+                        const bottomY = chart.scales['y-axis-0'].bottom
+                        ctx.save()
+                        ctx.setLineDash([10, 2]);
+                        ctx.beginPath()
+                        ctx.moveTo(x, topY)
+                        ctx.lineTo(x, bottomY)
+                        ctx.lineWidth = 3
+                        ctx.strokeStyle = '#FFDC5E'
+                        ctx.stroke()
+                        ctx.restore()
+                    }
+                }],
                 options: {
+                    spanGaps: true,
                     tooltips: {
                         callbacks: {
                            title: () => null,
@@ -35,14 +59,29 @@ export default function LineChart({data}) {
                      },
                     scales: {
                         xAxes: [{
+                            offset: true,
                             gridLines: {
                                 display: false
                             },
                             ticks: {
-                                display: false,
-                                autoSkip: true,
-                                maxTicksLimit: 20
+                                display: true,
+                                // autoSkip: true,
+                                maxTicksLimit: Infinity
                             },
+                            afterTickToLabelConversion: function(data){
+                                let xLabels = data.ticks
+                                xLabels.forEach((_, i) => {
+                                    if (i == xLabels.length-1){
+                                        xLabels[i] = 'Day X'
+                                    }
+                                    else if (i == xLabels.length-1-predictionGap) {
+                                        xLabels[i] = 'Day 0'
+                                    }
+                                    else {
+                                        xLabels[i] = ""
+                                    }
+                                })
+                            } 
                         }]
                     }
                 }
--- web/client/src/components/util/Lock.js
+++ web/client/src/components/util/Lock.js
@@ -1,18 +1,9 @@
 import React from 'react'
 
-export default function Lock({size}) {
+export default function Lock({size, price}) {
     return (
         <div className={`lock-icon lock-icon-${size ?? 'md'}`}>
-            <svg viewBox="0 0 128 128" width="64px" height="64px">
-                <path fill="#c7d6e2" d="M100,113H28c-2.761,0-5-2.239-5-5V56c0-2.761,2.239-5,5-5h72c2.761,0,5,2.239,5,5v52 C105,110.761,102.761,113,100,113z"/>
-                <path fill="#e7ecf3" d="M28,51c-2.761,0-5,2.239-5,5v26h82V56c0-2.761-2.239-5-5-5H28z"/>
-                <path fill="#f8e390" d="M97,39C97,20.775,82.225,6,64,6S31,20.775,31,39v12h12V39c0-11.598,9.402-21,21-21s21,9.402,21,21v12 h12V39z"/>
-                <path fill="#f6e5ab" d="M64 65A17 17 0 1 0 64 99A17 17 0 1 0 64 65Z"/>
-                <path fill="#cd928d" d="M64 74A8 8 0 1 0 64 90A8 8 0 1 0 64 74Z"/>
-                <path fill="none" stroke="#444b54" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="6" d="M100,113H28 c-2.761,0-5-2.239-5-5V56c0-2.761,2.239-5,5-5h72c2.761,0,5,2.239,5,5v52C105,110.761,102.761,113,100,113z"/>
-                <path fill="none" stroke="#444b54" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="6" d="M47.75 77C47.264 78.581 47 80.26 47 82c0 9.389 7.611 17 17 17s17-7.611 17-17-7.611-17-17-17c-1.74 0-3.419.264-5 .75M43 42v-3c0-11.598 9.402-21 21-21s21 9.402 21 21v3"/>
-                <path fill="none" stroke="#444b54" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="6" d="M31,42 v-3C31,20.775,45.775,6,64,6s33,14.775,33,33v3"/>
-            </svg>
+            
         </div>
     )
 }
