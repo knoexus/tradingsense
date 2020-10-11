55c8c7e62f81acb6dce6b219fe9653ac47ee07e2
knoexus <knoexus@yahoo.com>
2020-10-11 18:04:50 +0100

    fixed timer resume/pause in-render

--- web/client/src/components/util/game_sentinel/Timer.js
+++ web/client/src/components/util/game_sentinel/Timer.js
@@ -1,44 +1,52 @@
-import React, { Fragment } from 'react'
+import React, { useEffect } from 'react'
 import Timer from 'react-compound-timer'
 import { useQuery, useMutation } from '@apollo/client'
 import { QUERY_LOADING_MIXIN } from '../../../apollo-sm/queries'
 import { MUTATION_SET_ENDGAME } from '../../../apollo-sm/mutations'
 
-
-export default function NTimer({seconds}) {
+const withTimer = timerProps => WrappedComponent => wrappedComponentProps => (
+    <Timer {...timerProps}>
+      {timerRenderProps =>
+        <WrappedComponent {...wrappedComponentProps} timer={timerRenderProps} />}
+    </Timer>
+  )
+  
+const TimerWrapper = (props) => {
     const { data: { loadingMixin } }  = useQuery(QUERY_LOADING_MIXIN)
     const [ changeENDGM ] = useMutation(MUTATION_SET_ENDGAME)
+    useEffect(() => {
+        const { seconds, timer: { setTime }} = props
+        setTime(1000 * seconds)
+    }, [props.seconds])
+    useEffect(() => {
+        const { timer: { setCheckpoints } } = props
+        setCheckpoints([
+            {
+                time: 0,
+                callback: () => changeENDGM({
+                    variables: {
+                        newEndGame: true
+                    }
+                })
+            }
+        ])
+    })
+    useEffect(() => {
+        const { resume, pause } = props.timer
+        loadingMixin === true ? pause() : resume()
+    }, [loadingMixin])
     return (
         <div className="timer">
-            <Timer
-                initialTime={seconds * 1000}
-                direction="backward"
-                startImmediately={false}
-                checkpoints={[
-                    {
-                        time: 0,
-                        callback: () => changeENDGM({
-                            variables: {
-                                newEndGame: true
-                            }
-                        }),
-                    },
-                ]}
-            >
-                {({resume, pause}) => {
-                    loadingMixin && pause()
-                    !loadingMixin && resume()
-                    return (
-                        <Fragment>
-                            <h2>
-                                Time: <Timer.Minutes />
-                                :
-                                <Timer.Seconds formatValue={v => v.toString().length < 2 ? '0' + v : v} />
-                            </h2>
-                        </Fragment>
-                    )
-                }}
-            </Timer>
+            <h2>
+                Time: <Timer.Minutes />
+                :
+                <Timer.Seconds formatValue={v => v.toString().length < 2 ? '0' + v : v} />
+            </h2>
         </div>
     )
 }
+  
+export default withTimer({
+    direction: 'backward',
+    startImmediately: false
+})(TimerWrapper)
