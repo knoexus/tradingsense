350f7d14421dc3d3d97d830434ef1eaaab488c3a
knoexus <knoexus@yahoo.com>
2021-01-22 20:00:37 +0300

    side navbar

--- web/client/src/components/util/nav_elements/Header.js
+++ web/client/src/components/util/nav_elements/Header.js
@@ -1,22 +1,18 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+    const [isOpen, toggleOpen] = useState(false)
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/game">Game</Link>
-                    </li>
-                    <li>
-                        <Link to="/rules">Rules</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <Fragment>
+            <div className={`nav-toggler ${isOpen ? "nav-toggler-open" : ""}`} onClick={() => toggleOpen(!isOpen)}>
+                { [...Array(6).keys()].map(e => <span key={e}></span>) }
+            </div>
+            <div className={`sideNavPanel ${isOpen ? "sideNavPanel-open" : ""}`}>
+                <Link to="/">Home</Link>
+                <Link to="/game">Game</Link>
+                <Link to="/rules">Rules</Link>
+            </div>
+        </Fragment>
     )
 }
