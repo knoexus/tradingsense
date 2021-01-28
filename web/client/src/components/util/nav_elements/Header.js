a0047912fcc4dd13ee8a16a96e4f2e271c708604
knoexus <knoexus@yahoo.com>
2021-01-28 16:27:52 +0300

    nav link current page indication

--- web/client/src/components/util/nav_elements/Header.js
+++ web/client/src/components/util/nav_elements/Header.js
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
     const [isOpen, toggleOpen] = useState(false)
+    const pathName = window.location.pathname
     return (
         <Fragment>
             <div className={`nav-toggler ${isOpen ? "nav-toggler-open" : ""}`} onClick={() => toggleOpen(!isOpen)}>
                 { [...Array(6).keys()].map(e => <span key={e}></span>) }
             </div>
             <div className={`sideNavPanel ${isOpen ? "sideNavPanel-open" : ""}`}>
-                <Link to="/">Home</Link>
-                <Link to="/game">Game</Link>
-                <Link to="/rules">Rules</Link>
+                <Link to="/" className={pathName == "/" ? "sideNavPanel-link-active" : ""}>Home</Link>
+                <Link to="/game" className={pathName == "/game" ? "sideNavPanel-link-active" : ""}>Game</Link>
+                <Link to="/rules" className={pathName == "/rules" ? "sideNavPanel-link-active" : ""}>Rules</Link>
             </div>
         </Fragment>
     )
