knoexus/tradingsense

commit 9174bd3517e2fe4a2707c9c31ac957da421058e3

added extra check for dates: last (candle) = last (technical)

web/resolvers.js
@@ -92,7 +92,9 @@ exports.mixinResolver = mixinResolver = async (_, args, r=0, r_threshold=9) => {
         if (technicals == [] || candles == null) throw NULLRESPONSE
         const technicals_day0 = technicals[0]
         if (technicals_day0 == []) throw NULLRESPONSE
-        if (candles[candles.length-1].timestamp.toString() !== technicals_day0.t.toString()) throw DATAMISMATCH
+        if (candles[candles.length-1].timestamp.toString() !== technicals_day0.t.toString() 
+            || final_candle.timestamp.toString() !== technicals[technicals.length-1].t.toString()) 
+            throw DATAMISMATCH
         const technicals_all_prices = getTechnicalsAllPrice(technicals_day0.indicators.length)
 
         const minMaxStocks = minMaxStocksResolver(_, {

commit d7251cc5b7c8a65aeb7688bb7216847f810576ff

fixed error in mixin target price candle check condition introduced in 9174bd3

web/client/src/components/util/AmountSelectionModal.js
@@ -58,8 +58,6 @@ export default function AmountSelectionModal({ action, open, proceed, closeModal
   const [stocks, changeStocks] = useState(0)
   const [_open, setOpen] = useState(open)
 
-  console.log(open, _open)
-
   const [changePLP] = useMutation(MUTATION_SET_PROFIT_LOSS_PARAMS)
 
   const minStocks = minMaxStocks?.minMaxStocks?.minStocks

web/resolvers.js
@@ -92,8 +92,7 @@ exports.mixinResolver = mixinResolver = async (_, args, r=0, r_threshold=9) => {
         if (technicals == [] || candles == null) throw NULLRESPONSE
         const technicals_day0 = technicals[0]
         if (technicals_day0 == []) throw NULLRESPONSE
-        if (candles[candles.length-1].timestamp.toString() !== technicals_day0.t.toString() 
-            || final_candle.timestamp.toString() !== technicals[technicals.length-1].t.toString()) 
+        if (candles[candles.length-1].timestamp.toString() !== technicals_day0.t.toString())
             throw DATAMISMATCH
         const technicals_all_prices = getTechnicalsAllPrice(technicals_day0.indicators.length)
 
@@ -159,5 +158,6 @@ exports.profitLossResolver = profitLossResolver = async (_, args) => {
     const company_profile = await getCompanyProfileByID(args._id)
     const symbol = company_profile.ticker
     const candle = await getCandlesSingle(symbol, realDate)
+    console.log(candle)
     return candle.close
 }