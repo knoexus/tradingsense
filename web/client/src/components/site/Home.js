46901c12a0370b7a70ca7e321ae46b9878a5daff
knoexus <knoexus@yahoo.com>
2021-02-16 03:15:22 +0300

    full screen mode on play press (not within the game)

--- web/client/src/components/site/Home.js
+++ web/client/src/components/site/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../images/logo.png'
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import MenuBookIcon from '@material-ui/icons/MenuBook'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
 
 export default function Home() {
+    const openFullscreen = () => {
+        const elem = document.documentElement
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen()
+        } else if (elem.webkitRequestFullscreen) {
+          elem.webkitRequestFullscreen()
+        } else if (elem.msRequestFullscreen) {
+          elem.msRequestFullscreen()
+        }
+    }
     return (
         <div className="home">
             <div className="home-detail">
@@ -16,7 +26,7 @@ export default function Home() {
                 </div>
                 <div className="home-detail-buttons">
                     <Link to="/game">
-                        <button className="home-detail-button home-detail-button-play">
+                        <button onClick={() => openFullscreen()} className="home-detail-button home-detail-button-play">
                             <div>
                                 <span>Play</span>
                                 <PlayArrowIcon/>
