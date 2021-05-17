def2bd012c9fc1971a0aeb51e7f9d3124da7d67f
knoexus <knoexus@yahoo.com>
2021-05-17 18:06:16 +0300

    add poster images for rules videos

--- web/client/src/components/site/Rules.js
+++ web/client/src/components/site/Rules.js
@@ -4,6 +4,9 @@ import card from '../../videos/rules-card.mov'
 import technicals from '../../videos/rules-technicals-was.mov'
 import company_info from '../../images/rules-company-info.png'
 import modal from '../../videos/rules-modal.mov'
+import cardImg from '../../images/rules-card.png'
+import modalImg from '../../images/rules-modal.png'
+import technicalsImg from '../../images/rules-technicals-was.png'
 import Arrow from '../util/Arrow'
 import useIntersect from '../../hooks/useIntersect'
 
@@ -39,7 +42,7 @@ export default function Rules() {
             <div>
                 <IDiv className="rules-card">
                     <p><b>TradingSense</b> is a game which simulates mid-term trading in a fun way. The game introduces cards which represent assets and their trading information. As a good portion of this info is intentionally concealed, the cards can be manipulated with to get the maximum from the available data and help decide whether to buy or sell the asset.</p>
-                    <video className="rules-media-display" muted autoPlay loop src={card} type="video/mp4"></video>
+                    <video poster={cardImg} className="rules-media-display" muted autoPlay loop src={card} type="video/mp4"></video>
                 </IDiv>
                 <IDiv className="rules-chart">
                     <h2>Chart</h2>
@@ -53,7 +56,7 @@ export default function Rules() {
                     <p>The balance only accounts for how successful you trades are. However, a 3000$ improvised starting capital is given to unlock hidden stock info and prevent your balance from going negative too soon. With that said, if you still manage to go below 0, the game will be over.</p>
                     <div>
                         <img className="rules-media-display" src={company_info}></img>
-                        <video className="rules-media-display" muted autoPlay loop src={technicals} type="video/mp4"></video>
+                        <video poster={technicalsImg} className="rules-media-display" muted autoPlay loop src={technicals} type="video/mp4"></video>
                     </div>
                     <p>To help you make up your mind, info regarding the asset industry, or reveal its logo / name as well as technical indicators can be unlocked. With the latter you can select a day which you would like to unlock indicators for, but beware that its proximity to day X affects the price of an unlock. </p>
                 </IDiv>
@@ -67,7 +70,7 @@ export default function Rules() {
                         </div>
                     </div>
                     <div className="rules-buying-selling-modal-example">
-                        <video className="rules-media-display" muted autoPlay loop src={technicals} type="video/mp4" src={modal}></video>
+                        <video poster={modalImg} className="rules-media-display" muted autoPlay loop src={technicals} type="video/mp4" src={modal}></video>
                         <p>You will be prompted to select how much of the asset you would like to sell (given that you virtually own it) or buy. This stands for the risk you are willing to take: the more you buy or sell - the bigger is the potential profit / loss. Please mind that you do not need to worry about the asset price - buying and selling is totally virtual and what matters is whether you got the market action right and to what extent. </p>
                     </div>
                     <p className="rules-important">Actions of buying and selling might be confusing but in fact are not too difficult to understand. For the both the price on day 0 will be compared against price on day X, also accounting the number of assets you have selected. If you decide to <b className="rules-buy">BUY</b> an asset, the profit / loss of selling it on day X will be added / subtracted from your balance. If you decide to <b className="rules-sell">SELL</b> an asset, the profit / loss of not holding it until day X will be added / subtracted from your balance. </p>
