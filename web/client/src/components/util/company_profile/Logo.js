fcd97ac3ed581cf8df4840026ba77bad3ca131f8
knoexus <knoexus@yahoo.com>
2021-05-17 13:20:56 +0300

    fix infinitely loading logo (not loading logo error case)

--- web/client/src/components/util/company_profile/Logo.js
+++ web/client/src/components/util/company_profile/Logo.js
@@ -5,20 +5,14 @@ import { MUTATION_ADD_TO_CURRENT_POINTS } from '../../../apollo-sm/mutations'
 import LockedItem from '../LockedItem'
 import LogoSkeleton from '../../skeletons/company_profile/LogoSkeleton'
 import { Fragment } from 'react'
-
+import error_logo from '../../../images/error-logo.png'
 
 export default function Logo({data, fid}) {
     const [lock, changeLock] = useState(data.value === null)
     const [needFetch, changeFetchNeeded] = useState(false)
-    const [addToCP] = useMutation(MUTATION_ADD_TO_CURRENT_POINTS)
     const logoUnlockTry = () => {
         changeLock(!lock)
         changeFetchNeeded(!needFetch)
-        addToCP({
-            variables: {
-                addPoints: -data.price
-            }
-        })
     }
 
     const LI = <LockedItem handler="Logo" price={data.price} unlockTry={logoUnlockTry} extraClasses={["item-covered-companyProfile-logo"]}/>
@@ -28,35 +22,52 @@ export default function Logo({data, fid}) {
             { lock ? 
                 LI
             : needFetch ? 
-                <LogoGQL fid={fid} errorComponent={LI}></LogoGQL>
+                <LogoGQL fid={fid} price={data.price} errorComponent={LI}></LogoGQL>
             :
-                <LogoContent data={data.value}></LogoContent>
+                <LogoContent is_gql={false} data={data.value} price={0}></LogoContent>
             }
         </Fragment>
     )
 }
 
-const LogoGQL = ({fid, errorComponent}) => {
+const LogoGQL = ({fid, price, errorComponent}) => {
     const { loading, error, data } = useQuery(COMPANY_PROFILE_LOGO, {
         variables: { fid }
     })
-    if (loading) return <LogoContent loading={loading}/>
+    if (loading) return <LogoContent is_gql={true} loading={loading} price={price}/>
     if (error) {
         console.error(error) // or handle
         return errorComponent
     }
-    if (data) return <LogoContent data={data.company_profile.logo.value}/>
+    if (data) return <LogoContent is_gql={true} data={data.company_profile.logo.value} price={price}/>
 }
 
-const LogoContent = ({loading, data}) => {
+const LogoContent = ({loading, is_gql, data, price}) => {
     const [imgLoaded, changeImgLoaded] = useState(false)
+    const [imgError, changeImgError] = useState(false)
+    const [addToCP] = useMutation(MUTATION_ADD_TO_CURRENT_POINTS)
+    const setImgLoaded = () => {
+        changeImgLoaded(true)
+        if (is_gql) {
+            addToCP({
+                variables: {
+                    addPoints: -price
+                }
+            })
+        }
+    }
+    
     return (
         <Fragment>
-            { (loading || !imgLoaded) && <LogoSkeleton/> }
+            { ((loading || !imgLoaded) && !imgError) && <LogoSkeleton/> }
             { data && 
-                <div style={imgLoaded ? {} : { display: 'none' }} className="companyProfile-logo">
+                <div style={imgLoaded || imgError ? {} : { display: 'none' }} className="companyProfile-logo">
                     <div className="companyProfile-content-item">
-                        <img alt="Logo" src={data} onLoad={() => changeImgLoaded(true)}></img> 
+                        { !imgError ? 
+                            <img alt="Logo" src={data} onError={() => changeImgError(true)} onLoad={() => setImgLoaded()}></img> 
+                            :
+                            <img alt="Logo" src={error_logo}></img> 
+                        }
                     </div>
                 </div>
             }
